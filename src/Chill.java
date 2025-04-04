a5f8d67431e9839b044f19c6be562b6ebb72bae1
luke-dkk <luke_persson@yahoo.dk>
2025-04-04 11:37:39 +0200

    New ClassDiagram and

--- src/Chill.java
+++ src/Chill.java
@@ -1,2 +1,23 @@
+package util;
 public class Chill {
+    TextUI ui = new TextUI();
+    Chill(){}
+
+    public void startSequence(String msg){
+        //welcome to site msg
+        ui.displayMessage(msg);
+
+        int input = ui.promptNumeric("1: register as new user." + "\n" + "2: Login as existing user.");
+
+        if (input = 1){
+            registerUser();
+        } else if (input = 2){
+            login();
+        } else {
+            ui.displayMessage("invalid input, please try again");
+            startSequence("");
+
+        }
+
+    }
 }
