d900fe4759b6b13cd2251e980ffe208283e6ef3b
DannyShayH <HalawiDD@gmail.com>
2025-04-07 14:08:12 +0200

    Diagram v 2

--- src/Main.java
+++ src/Main.java
@@ -6,34 +6,9 @@ import java.util.ArrayList;
 public class Main {
     public static void main(String[] args) {
         Chill chill = new Chill();
-       chill.startSequence("Welcome!");
+        chill.startSequence("Welcome!");
 
-       /* FileIO io = new FileIO();
-        TextUI ui = new TextUI();
-        String[] test = io.readUser("data/userData/Danny.csv", 3);
-        for(int i = 0; i < 3; i++){
-            ui.displayMessage(test[i]);
-        }
-*/
     }
-
 }
-       /* LoadDataFromFile myLoader = new LoadDataFromFile();
-
-        //initier alle brugere
-        ArrayList <User> users = new ArrayList<User>();
-
-        users = myLoader.loadDataFromSource();
-
-        //Initier alle film.csv
-        ArrayList <Movies> movies = new ArrayList<Movies>();
-
-        LoadMovies myMovieLoader = new LoadMediaFromFIle();
-
-        movies = myMovieLoader.loadMedia();
-            }
-        }
-*/
-
 
 
